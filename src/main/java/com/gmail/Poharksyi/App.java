000d8cb27ce4d3cf97b442e9f60ca2739660e087
Poharskyi <avpog.box@gmail.com>
2024-11-04 19:41:13 +0100

    commit

--- src/main/java/com/gmail/Poharksyi/App.java
+++ src/main/java/com/gmail/Poharksyi/App.java
@@ -4,6 +4,7 @@ import static com.gmail.Poharksyi.Util.arraySearchAndRecreate;
 
 public class App
 {
+    //
     public static void main( String[] args ) {
         int[] arr = {5, 65, 2, 5, 12, 0};
         int[] newArr = arraySearchAndRecreate(arr, 5);
@@ -11,6 +12,4 @@ public class App
             System.out.println(num);
         }
     }
-
-
 }
