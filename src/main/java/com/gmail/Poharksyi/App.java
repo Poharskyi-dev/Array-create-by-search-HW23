aed811512597391955cb466bfd6898f80706ee7b
Poharskyi <avpog.box@gmail.com>
2024-11-05 14:32:36 +0100

    add code

---
+++ src/main/java/com/gmail/Poharksyi/App.java
@@ -0,0 +1,15 @@
+package com.gmail.Poharksyi;
+
+import static com.gmail.Poharksyi.Util.arraySearchAndRecreate;
+
+public class App
+{
+    //
+    public static void main( String[] args ) {
+        int[] arr = {5, 65, 2, 5, 12, 0};
+        int[] newArr = arraySearchAndRecreate(arr, 5);
+        for (int num : newArr){
+            System.out.println(num);
+        }
+    }
+}
